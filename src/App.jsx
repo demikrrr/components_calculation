658d91344b5a44f2ba3ad6f145f972bcc30e1711
Denis Krayushkinn <dkrayushkin@gmail.com>
2024-12-20 15:31:38 +0300

    ready

--- src/App.jsx
+++ src/App.jsx
@@ -2,70 +2,115 @@ import { useState } from 'react';
 import styles from './app.module.css';
 
 export const App = () => {
-  // const [count, setCount] = useState(0)
+  const [operand1, setOperand1] = useState('');
+  const [operand2, setOperand2] = useState('');
+  const [operator, setOperator] = useState('');
+  const [result, setResult] = useState('');
+  const [displayColor, setDisplayColor] = useState('#222');
+
+  const NUMS = [];
+  for (let i = 1; i < 10; i++) {
+    NUMS.push(i.toString());
+  }
+  NUMS.push('0');
+
+  const enterNumber = (num) => {
+    if (!operator) {
+      setOperand1((prevNum) => prevNum + num);
+    } else {
+      setOperand2((prevNum) => prevNum + num);
+    }
+  };
+
+  const addOperationSum = (num) => {
+    if (operand1 && !operand2) {
+      setOperator(num);
+    }
+  };
+
+  const addOperationSub = (num) => {
+    if (operand1 && !operand2) {
+      setOperator(num);
+    }
+  };
+
+  const clearDisplay = () => {
+    setOperand1('');
+    setOperand2('');
+    setOperator('');
+    setResult('');
+  };
+
+  const getResult = () => {
+    if (operand1 && operand2 && operator) {
+      const num1 = parseFloat(operand1);
+      const num2 = parseFloat(operand2);
+      let res = 0;
+      switch (operator) {
+        case '+':
+          res = num1 + num2;
+          break;
+        case '-':
+          res = num1 - num2;
+          break;
+        default:
+          break;
+      }
+      setResult(res.toString());
+      setDisplayColor('#28a745');
+    }
+  };
+
+  let summ = operand1 + operator + operand2;
 
   return (
     <>
       <div className={styles.calculator}>
-        <div className={styles.display}>0</div>
-        <div className={styles.buttons}>
-          <button className={styles.btn + ' ' + styles.btnNumber}> 0 </button>
-          <button className={styles.btn + ' ' + styles.btnNumber}> 1 </button>
-          <button className={styles.btn + ' ' + styles.btnNumber}> 2 </button>
-          <button className={styles.btn + ' ' + styles.btnNumber}> 3 </button>
-          <button className={styles.btn + ' ' + styles.btnNumber}> 4 </button>
-          <button className={styles.btn + ' ' + styles.btnNumber}> 5 </button>
-          <button className={styles.btn + ' ' + styles.btnNumber}> 6 </button>
-          <button className={styles.btn + ' ' + styles.btnNumber}> 7 </button>
-          <button className={styles.btn + ' ' + styles.btnNumber}> 8 </button>
-          <button className={styles.btn + ' ' + styles.btnNumber}> 9 </button>
+        <div className={styles.display} style={{ backgroundColor: displayColor }}>
+          {result ? result : summ}
+        </div>
 
-          <button className={styles.btn + ' ' + styles.btnOperator}>+</button>
-          <button className={styles.btn + ' ' + styles.btnOperator}>-</button>
-          <button className={styles.btn + ' ' + styles.btnOperator}>=</button>
-          <button className={styles.btn + ' ' + styles.btnOperator}>C</button>
+        <div className={styles.inputField}>
+          <div className={styles.numbers}>
+            {NUMS.map((num) => (
+              <button
+                key={num}
+                className={styles.btn + ' ' + styles.btnNumber}
+                onClick={() => enterNumber(num)}
+              >
+                {num}
+              </button>
+            ))}
+          </div>
+
+          <div className={styles.operators}>
+            <button
+              className={styles.btn + ' ' + styles.btnOperator}
+              onClick={() => addOperationSum('+')}
+            >
+              +
+            </button>
+            <button
+              className={styles.btn + ' ' + styles.btnOperator}
+              onClick={() => addOperationSub('-')}
+            >
+              -
+            </button>
+            <button
+              className={styles.btn + ' ' + styles.btnOperator}
+              onClick={() => getResult()}
+            >
+              =
+            </button>
+            <button
+              className={styles.btn + ' ' + styles.btnOperator}
+              onClick={() => clearDisplay()}
+            >
+              C
+            </button>
+          </div>
         </div>
       </div>
     </>
   );
 };
-
-// return (
-//   <div className={styles.container}>
-//     <div className={styles.card}>
-//       <h1>Инструкция по готовке пельменей</h1>
-//       <div className={styles.steps}>
-//         <div className={styles['steps-content']}>{steps[activeIndex].content}</div>
-//         <ul className={styles['steps-list']}>
-//           {steps.map(({ id, title }, index) => (
-//             <li
-//               className={`${styles['steps-item']} ${index < activeIndex ? styles.done : ''} ${index === activeIndex ? styles.active : ''}`}
-//               key={id}
-//             >
-//               <button
-//                 className={styles['steps-item-button']}
-//                 onClick={() => setActiveIndex(index)}
-//               >
-//                 {index + 1}
-//               </button>
-//               {title}
-//             </li>
-//           ))}
-//         </ul>
-//         <div className={styles['buttons-container']}>
-//           <button
-//             className={styles.button}
-//             disabled={inFirstStep}
-//             onClick={handleBackClick}
-//           >
-//             Назад
-//           </button>
-//           <button className={styles.button} onClick={handleNextClick}>
-//             {activeIndex < steps.length - 1 ? 'Далее' : 'Начать с начала'}
-//           </button>
-//         </div>
-//       </div>
-//     </div>
-//   </div>
-// );
-// };
